6ed35b1fda70bcc643163d0be678e73ee0ba1387
Atsushi Hatakeyama <hatake0919@yahoo.co.jp>
2020-10-25 15:35:58 +0900

    Ruby課題基礎 Q1~Q5 回答

--- task.rb
+++ task.rb
@@ -5,7 +5,8 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names << "斎藤"
+  p names
 end
 
 def q2
@@ -13,21 +14,22 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-
+  array = array1 + array2
+  p array
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  p numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.include?(nil) ? sports.compact! : sports
   # 以下は変更しないで下さい
   p sports
 end
@@ -37,7 +39,9 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  [array1, array2].each do |array|
+    p array.empty?
+  end
 end
 
 def q6
@@ -122,7 +126,7 @@ def q16
     { name: "satou", age: 22 },
     { name: "yamada", age: 12 },
     { name: "takahashi", age: 32 },
-    { name: "nakamura", age: 41 }
+    { name: "nakamura", age: 41 },
   ]
 
   # 以下に回答を記載
@@ -182,7 +186,6 @@ class Zoo
 
 end
 
-
 def q20
   # ここは変更しないで下さい（動物園・ユーザー情報は変更していただいてOKです）
   zoo = Zoo.new(name: "旭山動物園", entry_fee: { infant: 0, children: 400, adult: 800, senior: 500 })
@@ -191,7 +194,7 @@ def q20
     UserQ20.new(name: "たま", age: 3),
     UserQ20.new(name: "ゆたぼん", age: 10),
     UserQ20.new(name: "あじー", age: 32),
-    UserQ20.new(name: "ぎん", age: 108)
+    UserQ20.new(name: "ぎん", age: 108),
   ]
 
   users.each do |user|
