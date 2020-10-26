e43ac7af1fcb392f6b4407c18b0ae1feed4b74a0
atsushi0919 <hatake0919@yahoo.co.jp>
2020-10-26 15:48:53 +0900

    Ruby課題基礎 Q8 修正

--- task.rb
+++ task.rb
@@ -71,7 +71,11 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map(&:capitalize!)
+  # 変更前
+  # programming_languages = programming_languages.map(&:capitalize!)
+
+  # 変更後
+  programming_languages.map(&:capitalize!)
   upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
